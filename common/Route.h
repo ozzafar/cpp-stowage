7ca16bf3b5650ecf57cc3e0ba6cef0c47a26970c
yarden flori <yarden.flori@gmail.com>
2020-04-09 16:16:46 +0300

    route

--- common/Route.h
+++ common/Route.h
@@ -5,9 +5,16 @@
 #ifndef CPP_STOWAGE_ROUTE_H
 #define CPP_STOWAGE_ROUTE_H
 
+using namespace std;
+
+#include <list>
+#include <string>
 
 class Route {
-
+private:
+    std::list<string> ports;
+public:
+    Route();
 };
 
 
